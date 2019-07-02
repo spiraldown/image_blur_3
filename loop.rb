6b26b6f9e6622d88e1223adabe8caf466db044b4
spiraldown <47649528+spiraldown@users.noreply.github.com>
2019-07-01 21:57:19 -0600

    Delete loop.rb

--- loop.rb
+++
@@ -1,8 +0,0 @@
-d = 1
-
-counter = d#Variable for iteration called 'i' which starts at 1
-
-until counter > 5 #Until we've gone over 5
-  puts counter #Output the loop count
-  counter = counter + 1 #Add one to the loop count
-end #End the loop
